92a93c4f47ac005deaa02925ff6d203c24097057
Serhii Serdiuk <serduksergey90@gmail.com>
2019-11-21 14:32:05 +0200

    implement recyclerView

---
+++ app/src/main/java/com/sserdiuk/topreddit/screens/main/PostItemAdapter.kt
@@ -0,0 +1,33 @@
+package com.sserdiuk.topreddit.screens.main
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PostItemAdapter(val clickListener: ListItemClickListener) : RecyclerView.Adapter<PostItemAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    interface ListItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        override fun onClick(v: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+    }
+}
